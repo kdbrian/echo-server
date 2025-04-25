22dc4957cc8ca7aa3d2654e85c2c8706b2c91d9d
kidiga <juniorprogrammer09@gmail.com>
2025-04-25 14:31:56 +0300

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kdbrian/echo
+
+go 1.24.2
---
+++ main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatalf("Could not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := "Hello, World!\n"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
